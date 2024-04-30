f9338577b29357ed26551b668a8c12c8b81c7b70
sidewarehouse <yokokura@vos.nagaokaut.ac.jp>
2024-04-30 18:36:16 +0900

    ArcsMat版リミッタ実装

--- lib/Limiter.hh
+++ lib/Limiter.hh
@@ -3,10 +3,10 @@
 //!
 //! 任意の数値で入力を制限して出力
 //!
-//! @date 2021/08/02
+//! @date 2024/04/30
 //! @author Yokokura, Yuki
 //
-// Copyright (C) 2011-2021 Yuki YOKOKURA
+// Copyright (C) 2011-2024 Yuki YOKOKURA
 // This program is free software;
 // you can redistribute it and/or modify it under the terms of the FreeBSD License.
 // For details, see the License.txt file.
@@ -14,11 +14,21 @@
 #ifndef LIMITER
 #define LIMITER
 
+#include "ArcsMatrix.hh"
 #include "Matrix.hh"
 
 namespace ARCS {	// ARCS名前空間
 	double Limiter(const double input, const double limit);	//!< リミッタ
 	double Limiter(const double input, const double pos_limit, const double neg_limit);	//!< リミッタ
+	
+	//! @brief リミッタ(ArcsMatrix縦ベクトル版)
+	//! @tparam	M	縦ベクトルの高さ
+	//! @param[in,out]	inout	入出力ベクトル
+	//! @param[in]		limit	リミット値ベクトル
+	template<size_t M>
+	void Limiter(ArcsMat<M,1>& inout, const ArcsMat<M,1>& limit){
+		for(size_t i = 1; i <= M; ++i) inout[i] = Limiter(inout[i], limit[i]);
+	}
 	
 	//! @brief リミッタ(縦ベクトル版)
 	//! @tparam	M	縦ベクトルの高さ
